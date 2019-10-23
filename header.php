0d7f0d6c233e9e45cd7d019737f2aeee789a77cf
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2019-10-23 12:16:01 +0200

    updated with some light styling

--- header.php
+++ header.php
@@ -2,17 +2,22 @@
 declare(strict_types=1);
 
 require __DIR__.'/functions.php';
-require __DIR__.'/data.php';
+// require __DIR__.'/data.php';
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title></title>
+        <title>The Poolshaw Herald</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+
+    <div class="flex-master">
+        <div class="flex-child">
+        <h3>Welcome to The Poolshaw Herald</h3>
+        </div>
+    </div>
+
