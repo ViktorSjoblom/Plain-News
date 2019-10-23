d41907697415d7bdb069d9457c0d293a48b29a0a
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2019-10-23 11:30:32 +0200

    created header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__.'/functions.php';
+require __DIR__.'/data.php';
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
