9bb584c4eefb127750753a225d40399daf64459b
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2019-10-29 17:27:08 +0100

    changed style

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-require __DIR__.'/data.php';
-require __DIR__.'/functions.php';
+require __DIR__."/data.php";
+require __DIR__."/functions.php";
 ?>
 
 <!DOCTYPE html>
@@ -20,20 +20,35 @@ require __DIR__.'/functions.php';
     <div class="flex-header">
          <h3 class="header">The Poolshaw Herald</h3>
     </div>
-        
+
+    <div class="flex-navbar">
+<div class="topnav">
+  <a class="navBarItem0" href="header.php">Home</a>
+  <a class="navBarItem1"  href="authors.php">Authors</a>
+  <a class="navBarItem2" href="articles.php">Articles</a>
+</div>
+</div>
+
+    
+    
 <?php usort($articles, "sortArticle"); ?>
 
 <?php foreach ($articles as $article): ?>
 
     <div class="flex-article">
         <article class="article">
-            <h2><?php echo $article['title'] ?></h2>
-            <img class="articleimage" src="<?php echo $article['contentIMG'] ?>">
-            <p><?php echo $article['content'] ?></p>
-            <p>Published: <?php echo $article['publishDate'] ?></p>
-            <p>Author: <?php echo $article['author'] ?></p>
+            <div class="flex-article2">
+                <img class="articleimage" loading="lazy" src="<?php echo $article["contentIMG"] ?>">
+            </div>
+            <h2><?php echo $article["title"] ?></h2>
+            <p><?php echo $article["content"] ?></p>
+            <p>Published: <?php echo $article["publishDate"] ?></p>
+            <p>Author: <?php echo $article["author"] ?></p>
+            <p>Likes: <?php echo $article["likeCounter"] ?></p>
         </article>
     </div>
         
 
 <?php endforeach; ?>
+
+<?php require __DIR__."/footer.php";
